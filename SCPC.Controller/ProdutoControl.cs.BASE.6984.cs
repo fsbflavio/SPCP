6e90751b7f91e41fb05cb4ec7c6ba66e8ebe766d
Flavio Sidnei Baia <fsbflavio@outlook.com>
2012-11-10 19:46:48 -0200

    Merge branch 'master' of https://github.com/fsbflavio/SPCP
    Conflicts:
    	SCPC.Controller/ProdutoControl.cs
    	SCPC.View/obj/x86/Debug/DesignTimeResolveAssemblyReferences.cache
    	SCPC.suo

---
+++ SCPC.Controller/ProdutoControl.cs.BASE.6984.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPCP.Controler
+{
+    public class ProdutoControl
+    {
+        public void Incluir()
+        {
+ 
+        }
+    }
+}
